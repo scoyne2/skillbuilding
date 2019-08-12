875463a012832bf62528203aa5e5c3354fcc9ab6
Sean Coyne <scoyne@gopro.com>
2019-08-12 10:18:18 -0700

    cleaning for flake8

--- python/averagewordlength.py
+++ python/averagewordlength.py
@@ -1,4 +1,4 @@
-def average_word_length(sentence):
+def AverageWordLength(sentence):
     if len(sentence) == 0:
         return 0
 
@@ -8,9 +8,3 @@ def average_word_length(sentence):
         word_length.append(len(words[word]))
     print(word_length)
     return sum(word_length) / len(word_length)
-
-
-def test_average_word_length():
-    assert average_word_length("this is ok test") == 3
-    assert average_word_length("") == 0
-    assert average_word_length("supercalifragilisticexpialidocious") == 34
