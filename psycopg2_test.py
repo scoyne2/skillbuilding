c9ce9a9bbb39d7c5f7920127770d72692e9cbfbb
Sean Coyne <scoyne@gopro.com>
2019-08-12 09:19:19 -0700

    Implement linting, clean up bad python files

--- psycopg2_test.py
+++ psycopg2_test.py
@@ -3,11 +3,12 @@ import io
 import csv
 from urllib import request
 
-#connect to our postgress server
+# connect to our postgress server
 conn = psycopg2.connect(dbname="postgres", user="postgres")
 cursor = conn.cursor()
-#create table to save data in
-cursor.execute("""
+# create table to save data in
+cursor.execute(
+    """
 CREATE TABLE weather(FID integer PRIMARY KEY,
 YEAR integer,
 MONTH integer,
@@ -22,16 +23,20 @@ PRESSURE integer,
 CAT text,
 BASIN text,
 Shape_Leng text )
-""")
+"""
+)
 
-#download the raw data
-response = request.urlopen('https://dq-content.s3.amazonaws.com/251/storm_data.csv')
+# download the raw data
+response = request.urlopen("https://dq-content.s3.amazonaws.com/251/storm_data.csv")
 reader = csv.reader(io.TextIOWrapper(response))
-#skip header
+# skip header
 next(reader)
-#save data into table
+# save data into table
 for line in reader:
-    cursor.execute("INSERT INTO weather VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",line)
+    cursor.execute(
+        "INSERT INTO weather VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+        line,
+    )
 
-#close connection
+# close connection
 conn.close()
