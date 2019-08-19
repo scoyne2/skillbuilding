d8fa1c903acf9ca426efcac73e88cb46eaa87402
Sean Coyne <scoyne@gopro.com>
2019-08-19 16:09:56 -0700

    updating build spec

--- ecs_deploy.sh
+++ ecs_deploy.sh
@@ -2,4 +2,4 @@ ecs-cli configure --cluster airflow-project --default-launch-type FARGATE --conf
 
 ecs-cli up --force --cluster-config airflow-project --security-group sg-08d79347eb5180b05 --vpc vpc-0dcf325813702429d --subnets subnet-05e4552ae8ad6263e,subnet-0c6c2fdd59d944ab5 
 
-ecs-cli compose --project-name airflow-project service up --cluster-config airflow-project --create-log-groups
+ecs-cli compose --project-name airflow-project service up --cluster-config airflow-project --create-log-groups --load-balancer-name airflow-project-load-balancer
