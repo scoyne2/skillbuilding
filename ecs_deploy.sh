67839abc7e4846dc4e3e21baff8f620bf06573cd
Sean Coyne <scoyne@gopro.com>
2019-08-19 17:23:44 -0700

    load balancer working

--- ecs_deploy.sh
+++ ecs_deploy.sh
@@ -1,5 +1,7 @@
+#ecs-cli down --force --cluster airflow-project
+
 ecs-cli configure --cluster airflow-project --default-launch-type FARGATE --config-name airflow-project --region us-west-1
 
 ecs-cli up --force --cluster-config airflow-project --security-group sg-08d79347eb5180b05 --vpc vpc-0dcf325813702429d --subnets subnet-05e4552ae8ad6263e,subnet-0c6c2fdd59d944ab5 
 
-ecs-cli compose --project-name airflow-project service up --cluster-config airflow-project --create-log-groups --load-balancer-name airflow-project-load-balancer
+ecs-cli compose --project-name airflow-project service up --cluster-config airflow-project --create-log-groups --target-group-arn arn:aws:elasticloadbalancing:us-west-1:852056369035:targetgroup/airflow-target-group-2/b94ad9849e1d43f8 --container-name webserver --container-port 8080
