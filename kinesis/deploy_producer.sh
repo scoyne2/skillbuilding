4c17be7539f2859954a018ab0cc95c7e97dff3ef
scoyne2 <scoyne2@kent.edu>
2020-01-09 15:39:06 -0800

    Added S3 bucket and firehose creation

--- kinesis/deploy_producer.sh
+++ kinesis/deploy_producer.sh
@@ -11,3 +11,9 @@ aws kinesis create-stream --stream-name twitter-stream --shard-count 1 --profile
 ecs-cli configure --cluster kinesis-project --default-launch-type FARGATE --config-name kinesis-project --region us-west-2
 ecs-cli up --force --cluster-config kinesis-project --security-group sg-a6e79ce8 --vpc vpc-c955edb1 --subnets subnet-b36e7bca,subnet-b36e7bca
 ecs-cli compose --project-name kinesis-project service up --cluster-config kinesis-project --create-log-groups
+
+#create s3 bucket
+aws s3api create-bucket --bucket kinesis-twitter-bucket --region us-east-1 --profile personal
+
+#create firehose
+aws firehose create-delivery-stream --profile personal --delivery-stream-name twitter-firehose --delivery-stream-type KinesisStreamAsSource --kinesis-stream-source-configuration '{"KinesisStreamARN": "arn:aws:kinesis:us-west-2:852056369035:stream/twitter-stream",  "RoleARN": "arn:aws:iam::852056369035:role/kinesis_firehose_delivery_role"}' --s3-destination-configuration '{"RoleARN": "arn:aws:iam::852056369035:role/kinesis_firehose_delivery_role", "BucketARN": "arn:aws:s3:::kinesis-twitter-bucket"}' --region us-west-2
